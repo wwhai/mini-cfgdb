54f24b62b6a45a985290bc829c29f65b6e04ac17
wwhai <cnwwhai@gmail.com>
2022-10-08 20:52:52 +0800

    dev: fix makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 cc = gcc
 objects = cfgdb.o hashmap.o main.o
+
+.PHONY : cfgdb
 cfgdb : ${objects}
-	 ${cc} -o cfgdb ${objects}
+		${cc} -o cfgdb ${objects}
 
 .PHONY : clean
 clean :
-	-rm cfgdb ${objects}
+		rm cfgdb ${objects}
--- main.c
+++ main.c
@@ -6,17 +6,5 @@ int main(int argc, char const *argv[])
     Byte k1[] = "K1";
     Byte v1[] = "V1";
     Set(db, k1, v1);
-    Byte k2[] = "K2";
-    Byte v2[] = "VV2";
-    Set(db, k2, v2);
-    Byte k3[] = "K3";
-    Byte v3[] = "VVV3";
-    Set(db, k3, v3);
-    Byte k4[] = "K4444";
-    Byte v4[] = "VVV44444444444";
-    Set(db, k4, v4);
-    Get(db, k1);
-    Get(db, k2);
-    Get(db, k3);
     return 0;
 }
